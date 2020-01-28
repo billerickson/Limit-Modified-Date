d58e1b0c0a6b4000d1720c6b94b86f52e4b942a6
Davis Shaver <davisshaver@gmail.com>
2020-01-28 08:18:58 -0500

    Additional docbloc cleanup

--- limit-modified-date.php
+++ limit-modified-date.php
@@ -6,9 +6,7 @@
  * Version: 1.0.0
  * Author: Bill Erickson
  * Author URI: https://www.billerickson.net
- *
  */
-
 class Limit_Modified_Date {
 
 	/**
@@ -39,51 +37,52 @@ class Limit_Modified_Date {
 	 */
 	private $asset_version = '1.0';
 
+	/**
+	 * Instantiate the plugin.
+	 */
 	public function __construct() {
 		$this->nonce_key = $this->meta_key . '_nonce';
 
-		// Use original modified date
+		// Use original modified date.
 		add_action( 'wp_insert_post_data', [ $this, 'use_original_modified_date' ], 20, 2 );
 
-		// Checkbox in block editor
+		// Checkbox in block editor.
 		add_action( 'init', [ $this, 'register_post_meta' ] );
 		add_action( 'enqueue_block_editor_assets', [ $this, 'enqueue_block_editor_assets' ] );
 
-		// Checkbox in classic editor
+		// Checkbox in classic editor.
 		add_action( 'post_submitbox_misc_actions', [ $this, 'classic_editor_checkbox' ] );
 		add_action( 'save_post', [ $this, 'save_post' ] );
 
 	}
 
-	/*
+	/**
 	 * Use original modified date
 	 *
-	 * @param array Slashed post data
-	 * @param array Raw post data
-	 *
+	 * @param array $data Slashed post data.
+	 * @param array $postarr Raw post data.
 	 * @return array Slashed post data with modified post_modified and post_modified_gmt
 	 * */
 	public function use_original_modified_date( $data, $postarr ) {
 
-		// Block editor uses post meta
-		$use_original = get_post_meta( $postarr['ID'], $this->meta_key, true );
+		// Block editor uses post meta.
+		$use_original  = get_post_meta( $postarr['ID'], $this->meta_key, true );
 		$last_modified = get_post_meta( $postarr['ID'], $this->last_mod_meta_key, true );
 
 		if ( $use_original && $last_modified ) {
-
-			$data['post_modified'] = date( 'Y-m-d H:i:s', strtotime( $last_modified ) );
+			$data['post_modified']     = date( 'Y-m-d H:i:s', strtotime( $last_modified ) );
 			$data['post_modified_gmt'] = get_gmt_from_date( $data['post_modified'] );
-
-		// Classic editor
 		} else {
-
+			// Classic editor.
 			$use_original = isset( $_POST[ $this->meta_key ] ) ? '1' === $_POST[ $this->meta_key ] : false;
 			if ( $use_original ) {
 
-				if ( isset( $postarr['post_modified'] ) )
+				if ( isset( $postarr['post_modified'] ) ) {
 					$data['post_modified'] = $postarr['post_modified'];
-				if ( isset( $postarr['post_modified_gmt'] ) )
+				}
+				if ( isset( $postarr['post_modified_gmt'] ) ) {
 					$data['post_modified_gmt'] = $postarr['post_modified_gmt'];
+				}
 			}
 		}
 
@@ -97,7 +96,7 @@ class Limit_Modified_Date {
 		$args = [
 			'show_in_rest' => true,
 			'single'       => true,
-		);
+		];
 
 		register_meta( 'post', $this->meta_key, $args );
 		register_meta( 'post', $this->last_mod_meta_key, $args );
@@ -136,7 +135,7 @@ class Limit_Modified_Date {
 	}
 
 	/**
-	 * Determine whether a post type supports custom links.
+	 * Determine whether a post type supports limiting the modified date.
 	 *
 	 * @param string $type The post type to check.
 	 * @return bool Whether this post type supports custom links.
@@ -147,8 +146,7 @@ class Limit_Modified_Date {
 	}
 
 	/**
-	 * Checkbox in classic editor
-	 *
+	 * Checkbox in classic editor.
 	 */
 	public function classic_editor_checkbox() {
 		if ( ! $this->is_supported_post_type( get_post_type() ) ) {
@@ -156,7 +154,7 @@ class Limit_Modified_Date {
 		}
 
 		wp_nonce_field( $this->meta_key, $this->nonce_key );
-		$val = get_post_meta( $post->ID, $this->meta_key, true );
+		$val = get_post_meta( get_the_ID(), $this->meta_key, true );
 
 		echo '<div class="misc-pub-section">';
 			echo '<input type="checkbox" name="' . esc_attr( $this->meta_key ) . '" id="' . esc_attr( $this->meta_key ) . '" value="1"' . checked( $val, '1', false ) . ' />';
@@ -164,8 +162,12 @@ class Limit_Modified_Date {
 		echo '</div>';
 	}
 
+	/**
+	 * Save the original modified date if applicable.
+	 *
+	 * @param int $post_id The Post ID.
+	 */
 	public function save_post( $post_id ) {
-
 		if ( ! isset( $_POST['post_type'] ) ) {
 			return;
 		}
@@ -186,7 +188,7 @@ class Limit_Modified_Date {
 		}
 
 		if ( ! isset( $_POST['limit_modified_date'] ) ) {
-			// editing the post causes it to be purged anyway, so just remove the meta
+			// Editing the post causes it to be purged anyway, so just remove the meta.
 			delete_post_meta( $post_id, $this->meta_key );
 		} else {
 			if ( 1 === absint( $_POST['limit_modified_date'] ) ) {
